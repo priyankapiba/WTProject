a8c31cb191b7cfc3009d2567b4f89430bfa3501b
BootLook <73538645+priyankapiba@users.noreply.github.com>
2022-04-24 17:33:13 +0530

    Files uploaded

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-top">
+          <div class="container m-auto">
+            <a href="#" class="m-auto" style="text-decoration: none;">NAME: Priyanka Suresh Salunke PRN: F19111151 CLASS: TE Comp 1</a>
+          </div>
+</nav>
